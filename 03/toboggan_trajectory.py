84923ca8e666db1eeb309bcf2fa04f910f2a8f1d
Jan Doležal <jandolezal@email.cz>
2022-05-14 17:32:44 +0200

    Refactor and add part2 for day 3

--- 03/toboggan_trajectory.py
+++ 03/toboggan_trajectory.py
@@ -3,6 +3,10 @@ Day 3: Toboggan Trajectory
 https://adventofcode.com/2020/day/3
 """
 
+from functools import reduce
+from operator import mul
+
+
 def load_data(filename = '03/test_input.txt'):
     with open(filename) as f:
         raw_lines = [line.strip() for line in f.readlines()]
@@ -11,28 +15,52 @@ def load_data(filename = '03/test_input.txt'):
 
 
 
-def part1(data):
+def part1(data, row_step=1, col_step=3):
     num_rows = len(data)
     num_cols = len(data[0])
+    row = 0
     col = 0
+
     count = 0
 
-    for row in range(1, num_rows):
-        col += 3
-        try:
-            char = data[row][col]
-        except IndexError:
-            col -= num_cols
-            char = data[row][col]
+    while row < num_rows - 1:
+        row += row_step
+        col = (col + col_step) % num_cols
+        char = data[row][col]
         if char == 1:
             count += 1
 
     return count
 
-data = load_data()
-count = part1(data)
+
+def part2(data):
+    STEPS = [
+        (1,1),
+        (1,3),
+        (1,5),
+        (1,7),
+        (2,1),
+        ]
+    
+    tree_counts = []
+
+    for step in STEPS:
+        tree_counts.append(part1(data, *step))
+    return reduce(mul, tree_counts)
+
+
+
+test_data = load_data()
+
+count = part1(test_data)
 assert count == 7, "Part 1 does not work on test data"
+
+product = part2(test_data)
+assert product == 336, "Part 2 does not work on test data"
 
 data = load_data("03/input.txt")
 count = part1(data)
 print(count)
+
+product = part2(data)
+print(product)
