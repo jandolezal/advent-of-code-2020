9237433e2dd035d684dacb9c950326aced5fb146
Jan Doležal <jandolezal@email.cz>
2022-06-02 21:02:08 +0200

    Add part 2 for day 9

--- 09/encoding_error.py
+++ 09/encoding_error.py
@@ -19,11 +19,28 @@ def part1(data, size=5):
             return target_num
 
 
+def part2(data, wrong_num, size=2):
+    while True:
+        for i in range(len(data) - size):
+            current_list = data[i : i + size]
+            if sum(current_list) == wrong_num:
+                return min(current_list) + max(current_list)
+        size += 1
+
+
 # Part 1
 test_data = load_data()
-wrong_num = part1(test_data, size=5)
-assert wrong_num == 127, 'Part 1 does not work with test data.'
+test_wrong_num = part1(test_data, size=5)
+assert test_wrong_num == 127, 'Part 1 does not work with test data.'
 
 data = load_data('09/input.txt')
 wrong_num = part1(data, size=25)
 print(wrong_num)
+
+
+# Part 2
+test_list = part2(test_data, test_wrong_num)
+assert test_list == 62, 'Part 2 does not work with test data.'
+
+list_ = part2(data, wrong_num)
+print(list_)
